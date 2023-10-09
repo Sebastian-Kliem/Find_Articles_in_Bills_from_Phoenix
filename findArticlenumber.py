6c0bf6bdc5114bd390322f1053f124dc3b422434
sebastian <sebastiankliem.sk@gmail.com>
2023-10-09 12:47:10 +0200

    add function to copy the founded bills in a directory

--- findArticlenumber.py
+++ findArticlenumber.py
@@ -2,6 +2,8 @@
 
 import os
 import argparse
+import shutil
+
 from pypdf import PdfReader
 
 
@@ -39,21 +41,26 @@ def main():
     article_number = args.article_number
 
     main_directory = os.path.dirname(os.path.abspath(__file__))
-    directory = main_directory + "/Bills/"
+    bill_directory = main_directory + "/Bills/"
+    output_directory = main_directory + "/FoundBills/"
 
-    if not os.path.exists(directory):
-        print(f"Das Verzeichnis '{directory}' existiert nicht.")
+    if not os.path.exists(bill_directory):
+        print(f"Das Verzeichnis '{bill_directory}' existiert nicht.")
         return
 
-    print(f"Suche nach Artikelnummer '{article_number}' in PDF-Rechnungen im Verzeichnis '{directory}':")
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    print(f"Suche nach Artikelnummer '{article_number}' in PDF-Rechnungen im Verzeichnis '{bill_directory}':")
 
     found_bills = []
-    for filename in os.listdir(directory):
+    for filename in os.listdir(bill_directory):
         if filename.endswith(".pdf"):
-            pdf_file = os.path.join(directory, filename)
+            pdf_file = os.path.join(bill_directory, filename)
             scanned_pdf = find_article_number(pdf_file, article_number)
             if scanned_pdf:
                 found_bills.append(scanned_pdf)
+                shutil.copy(pdf_file, os.path.join(output_directory, filename))
 
     for items in found_bills:
         print(f"Rechnungsnummer {items['billing_number']} vom {items['date']} Dateinmae: {items['file']}")
